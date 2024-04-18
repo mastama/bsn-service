935e12b3c91f117f8c95a635f287c3cecfae749a
Singgih Pratama <singgih.vendor.pasti@pegadaian.co.id>
2024-04-18 16:02:05 +0700

    initialize project set up dependency

---
+++ src/main/java/id/co/anfal/bsn/BsnApplication.java
@@ -0,0 +1,13 @@
+package id.co.anfal.bsn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/anfal/bsn/BsnApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.anfal.bsn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
